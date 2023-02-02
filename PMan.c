896f82b9a81c799d7656e7f50b790dd995221b11
David Ward <davidmward@mail.com>
2023-02-02 14:46:56 -0800

    added boolean header

--- PMan.c
+++ PMan.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <readline/readline.h>
+#include <stdbool.h>
 
 int main()
 {
-        char *inpt;
-        int i = 0;
-        while ( i < 10 )
-        {
-        	inpt = readline("Enter text: ");
-        	printf("%s", inpt);
-			printf("\n");
-			++i;
-        }
-        return 0;
+	char *inpt;
+	bool running = true;
+	while (running)
+	{
+		inpt = readline("PMan: > ");
+		printf("%s", inpt);
+		printf("\n");
+	}
+	return 0;
 }
 
