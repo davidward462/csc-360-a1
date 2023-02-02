eb6dd21cdc57731ed6f01dc7699aecc239ef689e
David Ward <davidmward@mail.com>
2023-02-02 15:37:24 -0800

    added tokenize code

--- PMan.c
+++ PMan.c
@@ -3,15 +3,39 @@
 #include <readline/readline.h>
 #include <stdbool.h>
 
+// bg
+
+// bglist
+
+// bgkill
+
+// bgstop
+
+// bgstart
+
+// pstat
+
 int main()
 {
-	char *inpt;
+	char *input;
 	bool running = true;
+	const char delim[1] = " ";
 	while (running)
 	{
-		inpt = readline("PMan: > ");
-		printf("%s", inpt);
-		printf("\n");
+		input = readline("PMan: > ");
+		printf("%s\n", input);
+
+	    // tokenize input
+        char *token =  strtok(input, delim); // get first token
+        //printf("fist token: %s", token);
+	    
+	    while(token != NULL) { // while there is another token
+
+		    //printf("in while");
+		    printf("%s\n", token);
+		    
+		    token = strtok(NULL, delim); // why?
+	    }
 	}
 	return 0;
 }
