aa1f047ff49cb633575bdf78fb37e11888231c1a
David Ward <davidmward@mail.com>
2023-02-02 16:32:27 -0800

    added buffer

--- PMan.c
+++ PMan.c
@@ -22,20 +22,28 @@ int main()
 	const char delim[1] = " ";
 	char *tokenList[80]; // array of addresses to chars
 	int tokenListIndex = 0;
+	char buffer[80]; // for temporary string storage
 	while (running)
 	{
 		input = readline("PMan: > ");
 		printf("%s\n", input);
 
+		if(strcmp(input, "\0"))
+		{
+			input = "";
+		}
+
 	    // tokenize input
         char *token =  strtok(input, delim); // get first token
-        strcpy(tokenList[tokenListIndex], token);// store first token (the main command) here
+        printf("token: %s", token);
+        //strcpy(buffer, token); // seg fault
+        //strcpy(tokenList[tokenListIndex], token); // causes seg fault
 
 	    while(token != NULL)
 	    { // while there is another token
 
-		    //printf("in while");
-		    printf("%s\n", token);
+			//strcpy(tokenList[tokenListIndex++], token); // store token
+			
 		    token = strtok(NULL, delim); // why?
 	    }
 	}
