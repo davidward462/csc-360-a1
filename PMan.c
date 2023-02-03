611e4bda683fcdfdb33caa8157f63d439c8ccee6
David Ward <davidmward@mail.com>
2023-02-02 16:09:34 -0800

    added array of strings

--- PMan.c
+++ PMan.c
@@ -20,6 +20,8 @@ int main()
 	char *input;
 	bool running = true;
 	const char delim[1] = " ";
+	char *tokenList[80]; // array of addresses to chars
+	int tokenListIndex = 0;
 	while (running)
 	{
 		input = readline("PMan: > ");
@@ -27,16 +29,17 @@ int main()
 
 	    // tokenize input
         char *token =  strtok(input, delim); // get first token
-        //printf("fist token: %s", token);
-	    
-	    while(token != NULL) { // while there is another token
+        strcpy(tokenList[tokenListIndex], token);// store first token (the main command) here
+
+	    while(token != NULL)
+	    { // while there is another token
 
 		    //printf("in while");
 		    printf("%s\n", token);
-		    
 		    token = strtok(NULL, delim); // why?
 	    }
 	}
+	
 	return 0;
 }
 
