d9b8b5913d95424b07fadd70e2b663eaf8b7fcea
David Ward <davidmward@mail.com>
2023-02-03 10:49:01 -0800

    copied and renamed PMan.c

--- PMan.c
+++ PMan.c
@@ -3,6 +3,8 @@
 #include <readline/readline.h>
 #include <stdbool.h>
 
+#define TOKEN_LEN 20
+
 // bg
 
 // bglist
@@ -17,35 +19,22 @@
 
 int main()
 {
-	char *input;
-	bool running = true;
-	const char delim[1] = " ";
-	char *tokenList[80]; // array of addresses to chars
-	int tokenListIndex = 0;
-	char buffer[80]; // for temporary string storage
-	while (running)
+	char *input; // from user
+	const char *delimeter = " ";
+	char *token;
+	char *tokenList[5][TOKEN_LEN];
+	//char *tokenList[20];
+
+	while(1)
 	{
 		input = readline("PMan: > ");
-		printf("%s\n", input);
 
-		if(strcmp(input, "\0"))
-		{
-			input = "";
-		}
+		printf("%s", input); // print input for debugging
+		token = strtok(input, delimeter); //
 
-	    // tokenize input
-        char *token =  strtok(input, delim); // get first token
-        printf("token: %s", token);
-        //strcpy(buffer, token); // seg fault
-        //strcpy(tokenList[tokenListIndex], token); // causes seg fault
-
-	    while(token != NULL)
-	    { // while there is another token
-
-			//strcpy(tokenList[tokenListIndex++], token); // store token
-			
-		    token = strtok(NULL, delim); // why?
-	    }
+		strncpy(*tokenList[0], token, TOKEN_LEN);
+		
+		printf("%s", *tokenList[0]);
 	}
 	
 	return 0;
